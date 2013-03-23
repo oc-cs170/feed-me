20ea5246ba2e9ab7627f2db94dee999286962a21
Joshua Reynolds <reyno28@email.lcc.edu>
2013-03-23 00:05:57 -0400

    edited the giant.py

--- giant.py
+++ giant.py
@@ -2,6 +2,11 @@ import pygame
 
 class Giant(object):
 	"""docstring for Giant"""
-	def __init__(self, arg):
+	def __init__(self, background, png='Planetcute PNG/Enemy Bug.png'):
+		self.background = background
+		#Initialize Sprite
 		pygame.sprite.Sprite.__init__(self)
-		self.arg = arg
+	
+		# Image place holder for Giant
+		self.image = pygame.image.load(png).convert_alpha()
+		self.rect = self.image.get_rect(topleft=(20, 20))
