1545d61ae0d161247651df07fe5cbdf324628178
Jared R. Luellen <Jared.Luellen@cs.olivetcollege.edu>
2013-03-22 23:03:26 -0400

    beginning of splash screen and time counting

---
+++ feed-me.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""Main file with game loop for PyGame.
+"""
+
+import pygame
+
+WINDOW_TITLE = 'PyGame'
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 950
+FPS = 30
+
+
+
+class PyGame(object):
+    """Create a game of PyGame."""
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption(WINDOW_TITLE)
+
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+        # Use a clock to control frame rate
+        self.clock = pygame.time.Clock()
+
+    def splashScreen(beginning):
+        if beginning == True:
+            #Show the screen
+            self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+            
+    
+
+    def play(self):
+        """Start PyGame program.
+        """
+
+        running = True
+        while running:
+            self.clock.tick(FPS)  # Max frames per second
+
+            # Event handling
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
+                    running = False
+
+            # Draw the scene
+            self.screen.fill((0, 0, 0))
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    game = PyGame()
+    game.play()
